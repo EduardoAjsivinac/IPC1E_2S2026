fbb3642f7948c91062431111e7e38051a63a7ec9
oscarinhp <3594457480101@ingenieria.usac.edu.gt>
2026-08-18 22:39:23 -0600

    New Commit 1: tablero + perímetro + entrada/salida + menú.

--- Tareas/Estacionamiento.java
+++ Tareas/Estacionamiento.java
@@ -1,116 +1,187 @@
 package Tareas;
-
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Estacionamiento {
-    // Tablero de 10x10
-    static String[][] tablero = new String[10][10];
-    // Matriz interna de 8x8 para placas
-    static String[][] estacionamiento = new String[8][8];
-    
-    // Coordenadas del perímetro (36 celdas)
-    static int[][] perimetro = new int[36][2];
-    static int idxEntrada = -1;
-    static int idxSalida = -1;
 
-    static Scanner scanner = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
     static Random random = new Random();
 
-    public static void main(String[] args) {
-        inicializarPerimetro();
-        generarEntradaSalida();
-        inicializarTablero();
+    static String[][] tablero = new String[10][10];
+    static int[][] perimetro = new int[36][2];
 
-        int opcion = 0;
+    static int entrada;
+    static int salida;
+
+    public static void main(String[] args) {
+
+        crearPerimetro();
+        crearEntradaSalida();
+        iniciarTablero();
+
+        int opcion;
+
         do {
-            mostrarMenu();
-            if (scanner.hasNextInt()) {
-                opcion = scanner.nextInt();
-                scanner.nextLine(); // Limpiar buffer
-                switch (opcion) {
-                    case 3 -> mostrarEstacionamiento();
-                    case 7 -> System.out.println("¡Gracias por utilizar el sistema!");
-                    case 1, 2, 4, 5, 6 -> System.out.println("Funcionalidad en desarrollo...");
-                    default -> System.out.println("Opción inválida. Intente de nuevo.");
-                }
-            } else {
-                System.out.println("Error: Ingrese un número válido.");
-                scanner.nextLine();
+            menu();
+            opcion = leerEntero("Seleccione una opcion: ");
+
+            switch (opcion) {
+
+                case 1:
+                    System.out.println("Ingreso de vehiculos: pendiente.");
+                    break;
+
+                case 2:
+                    System.out.println("Retiro de vehiculos: pendiente.");
+                    break;
+
+                case 3:
+                    mostrarTablero();
+                    break;
+
+                case 4:
+                    System.out.println("Busqueda: pendiente.");
+                    break;
+
+                case 5:
+                    System.out.println("Ruta: pendiente.");
+                    break;
+
+                case 6:
+                    System.out.println("Ingresos: pendiente.");
+                    break;
+
+                case 7:
+                    System.out.println("Programa finalizado.");
+                    break;
+
+                default:
+                    System.out.println("Opcion no valida.");
             }
+
         } while (opcion != 7);
     }
 
-    static void inicializarPerimetro() {
-        int idx = 0;
-        for (int c = 0; c < 10; c++) { perimetro[idx][0] = 0; perimetro[idx++][1] = c; } // Arriba
-        for (int r = 1; r < 10; r++) { perimetro[idx][0] = r; perimetro[idx++][1] = 9; } // Derecha
-        for (int c = 8; c >= 0; c--) { perimetro[idx][0] = 9; perimetro[idx++][1] = c; } // Abajo
-        for (int r = 8; r >= 1; r--) { perimetro[idx][0] = r; perimetro[idx++][1] = 0; } // Izquierda
-    }
+    static void menu() {
 
-    static void generarEntradaSalida() {
-        int[] esquinas = {0, 9, 18, 27};
-        do {
-            idxEntrada = random.nextInt(36);
-        } while (esEsquina(idxEntrada, esquinas));
-
-        do {
-            idxSalida = random.nextInt(36);
-        } while (esEsquina(idxSalida, esquinas) || idxSalida == idxEntrada);
-    }
-
-    static boolean esEsquina(int idx, int[] esquinas) {
-        for (int e : esquinas) {
-            if (idx == e) return true;
-        }
-        return false;
-    }
-
-    static void inicializarTablero() {
-        for (int r = 0; r < 10; r++) {
-            for (int c = 0; c < 10; c++) {
-                if (r == 0 || r == 9 || c == 0 || c == 9) {
-                    tablero[r][c] = "=";
-                } else {
-                    tablero[r][c] = "L";
-                    estacionamiento[r - 1][c - 1] = null;
-                }
-            }
-        }
-        tablero[perimetro[idxEntrada][0]][perimetro[idxEntrada][1]] = "E";
-        tablero[perimetro[idxSalida][0]][perimetro[idxSalida][1]] = "S";
-    }
-
-    static void mostrarMenu() {
         System.out.println("\n===== SISTEMA DE ESTACIONAMIENTO =====");
-        System.out.println("1. Ingresar vehículo");
-        System.out.println("2. Retirar vehículo");
+        System.out.println("1. Ingresar vehiculo");
+        System.out.println("2. Retirar vehiculo");
         System.out.println("3. Mostrar estacionamiento");
-        System.out.println("4. Buscar vehículo por placa");
-        System.out.println("5. Mostrar ruta más corta entre entrada y salida");
+        System.out.println("4. Buscar vehiculo");
+        System.out.println("5. Mostrar ruta mas corta");
         System.out.println("6. Mostrar ingresos");
         System.out.println("7. Salir");
-        System.out.print("Seleccione una opción: ");
     }
 
-    static void mostrarEstacionamiento() {
-        System.out.println("\n   1 2 3 4 5 6 7 8");
-        int libres = 0, ocupados = 0;
+    static void crearPerimetro() {
 
-        for (int r = 0; r < 10; r++) {
-            if (r >= 1 && r <= 8) System.out.print(r + " ");
-            else System.out.print("  ");
+        int x = 0;
+
+        // Parte superior
+        for (int c = 0; c < 10; c++) {
+            perimetro[x][0] = 0;
+            perimetro[x][1] = c;
+            x++;
+        }
+
+        // Parte derecha
+        for (int f = 1; f < 10; f++) {
+            perimetro[x][0] = f;
+            perimetro[x][1] = 9;
+            x++;
+        }
+
+        // Parte inferior
+        for (int c = 8; c >= 0; c--) {
+            perimetro[x][0] = 9;
+            perimetro[x][1] = c;
+            x++;
+        }
+
+        // Parte izquierda
+        for (int f = 8; f >= 1; f--) {
+            perimetro[x][0] = f;
+            perimetro[x][1] = 0;
+            x++;
+        }
+    }
+
+    static void crearEntradaSalida() {
+
+        do {
+            entrada = random.nextInt(36);
+        } while (esEsquina(entrada));
+
+        do {
+            salida = random.nextInt(36);
+        } while (esEsquina(salida) || salida == entrada);
+    }
+
+    static boolean esEsquina(int posicion) {
+
+        return posicion == 0 ||
+               posicion == 9 ||
+               posicion == 18 ||
+               posicion == 27;
+    }
+
+    static void iniciarTablero() {
+
+        for (int f = 0; f < 10; f++) {
 
             for (int c = 0; c < 10; c++) {
-                System.out.print(tablero[r][c] + " ");
-                if (r >= 1 && r <= 8 && c >= 1 && c <= 8) {
-                    if (tablero[r][c].equals("L")) libres++;
-                    else if (tablero[r][c].equals("A")) ocupados++;
+
+                if (f == 0 || f == 9 || c == 0 || c == 9) {
+                    tablero[f][c] = "=";
+                } else {
+                    tablero[f][c] = "L";
                 }
             }
+        }
+
+        tablero[perimetro[entrada][0]][perimetro[entrada][1]] = "E";
+        tablero[perimetro[salida][0]][perimetro[salida][1]] = "S";
+    }
+
+    static void mostrarTablero() {
+
+        System.out.println("\n  1 2 3 4 5 6 7 8");
+
+        for (int f = 0; f < 10; f++) {
+
+            if (f >= 1 && f <= 8) {
+                System.out.print(f + " ");
+            } else {
+                System.out.print("  ");
+            }
+
+            for (int c = 0; c < 10; c++) {
+                System.out.print(tablero[f][c] + " ");
+            }
+
             System.out.println();
         }
-        System.out.println("Espacios libres: " + libres + " | Espacios ocupados: " + ocupados);
+    }
+
+    static int leerEntero(String mensaje) {
+
+        while (true) {
+
+            System.out.print(mensaje);
+
+            if (sc.hasNextInt()) {
+
+                int numero = sc.nextInt();
+                sc.nextLine();
+
+                return numero;
+
+            } else {
+
+                System.out.println("Ingrese un numero entero.");
+                sc.nextLine();
+            }
+        }
     }
 }
